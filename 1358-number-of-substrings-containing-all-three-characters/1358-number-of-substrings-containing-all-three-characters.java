e6bb23d0f5e95718946284ebd3a978c46c207aaa
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2025-12-10 18:51:58 +0530

    Time: 13 ms (66.4%), Space: 46.1 MB (54.15%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,18 +1,33 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        int[] freq = new int[3];
-        int left = 0, count = 0;
+        // int[] freq = new int[3];
+        // int left = 0, count = 0;
 
-        for (int right = 0; right < s.length(); right++) {
-            freq[s.charAt(right) - 'a']++;
+        // for (int right = 0; right < s.length(); right++) {
+        //     freq[s.charAt(right) - 'a']++;
 
-            while (freq[0] > 0 && freq[1] > 0 && freq[2] > 0) {
-                count += (s.length() - right);
-                freq[s.charAt(left) - 'a']--;
-                left++;
-            }
+        //     while (freq[0] > 0 && freq[1] > 0 && freq[2] > 0) {
+        //         count += (s.length() - right);
+        //         freq[s.charAt(left) - 'a']--;
+        //         left++;
+        //     }
+        // }
+
+        // return count;
+        int n=s.length();
+        int count =0;
+
+        int lastseen[] = new int[]{-1, -1, -1};
+
+        for(int i=0;i<s.length();i++){
+            lastseen[s.charAt(i)-'a']=i;
+            if(lastseen[0] != -1 && lastseen[1] != -1 && lastseen[2] != -1) {
+    int mini = Math.min(lastseen[0], Math.min(lastseen[1], lastseen[2]));
+    count += (1 + mini);
+}
+
         }
-
         return count;
+
     }
 }
