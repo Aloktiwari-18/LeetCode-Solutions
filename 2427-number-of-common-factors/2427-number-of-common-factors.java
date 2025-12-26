c340bbd33fe5ecd79cc21594b6cc40a1a3b028a1
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2025-12-26 23:31:54 +0530

    Time: 1 ms (60.55%), Space: 42.4 MB (14.25%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.java
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -7,7 +7,7 @@ class Solution {
         }else{
             min=b;
         }
-        for(int i=1;i<min+1;i++){
+        for(int i=1;i<=min;i++){
             if(a%i==0 && b%i==0){
                 count++;
             }
