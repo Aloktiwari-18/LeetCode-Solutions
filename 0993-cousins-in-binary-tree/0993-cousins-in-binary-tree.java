64d63a0710c30942141f244c5f7f8f2834e1d54f
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-01-05 16:14:49 +0530

    Time: 1 ms (26.44%), Space: 43.3 MB (18.88%) - LeetHub

---
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    static class Pair {
+        TreeNode node;
+        TreeNode parent;
+
+        Pair(TreeNode node, TreeNode parent) {
+            this.node = node;
+            this.parent = parent;
+        }
+    }
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        if (root == null) return false;
+
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(root, null));
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+
+            TreeNode parentX = null;
+            TreeNode parentY = null;
+
+            for (int i = 0; i < size; i++) {
+                Pair curr = q.poll();
+                TreeNode node = curr.node;
+
+                if (node.val == x) parentX = curr.parent;
+                if (node.val == y) parentY = curr.parent;
+
+                if (node.left != null)
+                    q.offer(new Pair(node.left, node));
+
+                if (node.right != null)
+                    q.offer(new Pair(node.right, node));
+            }
+
+            if (parentX != null && parentY != null) {
+                return parentX != parentY;
+            }
+
+            if (parentX != null || parentY != null) {
+                return false;
+            }
+        }
+
+        return false;
+    }
+}
