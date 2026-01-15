462a465a1d71b57e2e12a843229b75a7313f783f
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-01-16 01:37:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -1,40 +1,13 @@
-import java.util.*;
-
 class Solution {
     public int findCenter(int[][] edges) {
+int a= edges[0][0];
+int b= edges[0][1];
+int c= edges[1][0];
+int d= edges[1][1];
 
-        int n = edges.length + 1;
-        List<List<Integer>> adj = new ArrayList<>();
+if (a==c || b==d) return a;
+return b;
 
-        for (int i = 0; i <= n; i++) {
-            adj.add(new ArrayList<>());
-        }
-
-        for (int[] e : edges) {
-            adj.get(e[0]).add(e[1]);
-            adj.get(e[1]).add(e[0]);
-        }
-
-        boolean[] vis = new boolean[n + 1];
-        Queue<Integer> q = new LinkedList<>();
-
-        q.add(edges[0][0]); 
-        vis[edges[0][0]] = true;
-
-        while (!q.isEmpty()) {
-            int node = q.poll();
-
-            if (adj.get(node).size() == n - 1) {
-                return node;
-            }
-
-            for (int nei : adj.get(node)) {
-                if (!vis[nei]) {
-                    vis[nei] = true;
-                    q.add(nei);
-                }
-            }
-        }
-        return -1;
+        
     }
 }
