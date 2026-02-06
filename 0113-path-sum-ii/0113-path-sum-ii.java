05f865189ef839fa8e1af594e40912f8c4575490
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-02-06 11:45:41 +0530

    Time: 1 ms (99.89%), Space: 45.6 MB (37.24%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -14,30 +14,31 @@
  * }
  */
 class Solution {
-
-    
-    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List<List<Integer>> ans= new ArrayList<>();
-        if(root==null) return ans;
-        dfs(root,targetSum,new ArrayList<>(),ans);
-        return ans;
-        
-    }
-
-    private void dfs(TreeNode node, int sum, List<Integer> path, List<List<Integer>> ans){
-        if(node==null){
+    public void findPath(TreeNode root,int sum,List<Integer> path, List<List<Integer>> ans){
+        if(root==null){
             return ;
         }
-        path.add(node.val);
-        sum-=node.val;
-        if(node.left==null && node.right==null && sum==0){
+        sum-=root.val;
+        path.add(root.val);
+        if(root.left==null && root.right==null && sum==0){
             ans.add(new ArrayList<>(path));
-
         }
-        dfs(node.left,sum,path,ans);
-        dfs(node.right,sum,path,ans);
+        findPath(root.left, sum, path, ans);
+        findPath(root.right, sum, path, ans);
+        // Backtracking
         path.remove(path.size()-1);
+        return;
 
 
+
+    }
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        
+        List<List<Integer>> ans= new ArrayList<>();
+        if(root==null) return ans;
+        
+        findPath(root, targetSum, new ArrayList<>(), ans);
+        return ans;
+        
     }
 }
