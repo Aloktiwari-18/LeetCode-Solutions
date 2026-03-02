89bdbd6c0f0a8528beaec182ed9fd9356f16a14c
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-03-02 18:04:07 +0530

    Time: 27 ms (27.72%), Space: 47.7 MB (8.52%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,32 +1,25 @@
 class Solution {
-    public long minCoin(int i, int [] nums, int amount, int [][] dp){
-        if (i== nums.length) {
-            if(amount==0){
-                return 0;
-            }
-            else return Integer.MAX_VALUE;
-        }
-        
-        if(dp[i][amount]!=-1) return dp[i][amount];
 
-        long skip= minCoin(i+1, nums, amount, dp);
-        if(amount- nums[i]<0) return dp[i][amount]= (int)skip;
-        long take=1+ minCoin(i, nums, amount-nums[i], dp);
-        return dp[i][amount]=(int ) Math.min(take, skip);
-        
-    }
     public int coinChange(int[] coins, int amount) {
         int N= coins.length;
-        int dp[][]= new int [N+1][amount+1];
+        long dp[][]= new long [N][amount+1];
        
 
-        for(int i=0;i<N;i++){
+        for(int i=0;i<dp.length;i++){
             for(int j=0;j<dp[0].length;j++){
-                dp[i][j]=-1;
+              
+                long skip= (i>0) ? dp[i-1][j] : ((j==0)? 0 : Integer.MAX_VALUE);
+                if(j- coins[i]<0)  dp[i][j]= (int)skip;
+                else{
+                    long take=1+ dp[i][j-coins[i]];
+                    dp[i][j]=(int)Math.min(take, skip);
+
+                }
+                
 
             }
         }
-      int ans=(int) minCoin(0, coins, amount,dp);
+      int ans=(int) dp[N-1][amount];
       if(ans==Integer.MAX_VALUE) return -1;
       return ans;
 
