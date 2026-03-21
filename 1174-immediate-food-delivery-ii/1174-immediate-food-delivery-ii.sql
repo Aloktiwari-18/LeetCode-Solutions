fd82842a383f878c178d83ddd28c180c85848d7b
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-03-21 12:23:15 +0530

    Time: 629 ms (43.44%), Space: 0B (100%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select round(avg(temp.order_date=temp.customer_pref_delivery_date) * 100, 2) as immediate_percentage
+from (select *, RANK() OVER(partition by customer_id order by order_date) as r from Delivery) as temp
+where temp.r = 1
