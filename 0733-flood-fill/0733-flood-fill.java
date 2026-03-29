a5a22ed88b0863274c984419b66dbc052936d636
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-03-30 01:53:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,24 +1,22 @@
 class Solution {
-    public static void dfs(int i,int j,int[][] image,int oldColor,int newColor){
-        int n= image.length;
-        int m= image[0].length;
-        if(i<0 || j<0 || i>=n || j>=m || image[i][j]!=oldColor){
+    public static void dfs(int [][] image, int sr, int sc, int color, int oldColor){
+          int n= image.length;
+          int m= image[0].length;
+          if(sr<0 || sc<0 || sr>=n || sc>=m || image[sr][sc]!=oldColor){
             return ;
         }
-        image[i][j]=newColor;
-        dfs(i - 1, j, image,oldColor,newColor); // up
-        dfs(i + 1, j, image,oldColor,newColor); // down
-        dfs(i, j - 1, image,oldColor,newColor); // left
-        dfs(i, j + 1, image,oldColor,newColor);
+        image[sr][sc]= color;
+        dfs(image,sr+1,sc,color, oldColor);
+        dfs(image,sr+1,sc,color, oldColor);
 
 
     }
-   
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int oldColor= image[sr][sc];
-        if(oldColor==color) return image;
-        dfs(sr,sc,image,oldColor,color);
+        if(oldColor== color) return image;
+        dfs(image, sr, sc, color, oldColor);
         return image;
+
         
     }
 }
