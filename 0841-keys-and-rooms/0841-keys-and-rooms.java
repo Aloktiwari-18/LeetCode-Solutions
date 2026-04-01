a09bf46828d39d62ae3ec023743edd6269c5fe84
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-04-01 12:17:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,22 +1,29 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        boolean[] seen = new boolean[rooms.size()];
-        seen[0] = true;
-        Stack<Integer> stack = new Stack();
-        stack.push(0);
-
+        int n= rooms.size();
+        Stack<Integer> st= new Stack<>();
+        st.push(0);
         
-        while (!stack.isEmpty()) {
-            int node = stack.pop(); 
-            for (int nei: rooms.get(node)) 
-                if (!seen[nei]) {
-                    seen[nei] = true;
-                    stack.push(nei);
-                }
-        }
+        boolean vis[]= new boolean [n];
+        vis[0]= true;
+        while(!st.isEmpty()){
+            int room= st.pop();
+            
+            for(int neighbor: rooms.get(room)){
+                if(vis[neighbor]==false){
+                st.push(neighbor);
+                vis[neighbor]=true;
 
-        for (boolean v: seen)  
-            if (!v) return false;
-        return true;
+                }
+
+                
+            }
+        }
+        for(boolean v : vis){
+            if(!v) return false;
+        }
+return true;
+        
+        
     }
 }
