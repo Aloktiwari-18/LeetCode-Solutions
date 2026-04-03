64f005f0095b5f00e4a4236ea2588cc113e1515e
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-04-03 11:16:25 +0530

    Time: 1 ms (84.95%), Space: 45.4 MB (71.2%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,35 +1,31 @@
 class Solution {
-    public static void allPermute(int nums[], List<Integer> ds,List<List<Integer>> ans, boolean [] freq){
-
-    
-    if(ds.size()==nums.length){
-        ans.add(new ArrayList<>(ds));
-        return;
-    }
-    for(int i=0;i<nums.length;i++){
-        if(!freq[i]){
-            freq[i]=true;
-            ds.add(nums[i]);
-            allPermute(nums, ds,ans,freq);
-            ds.remove(ds.size()-1);
-            freq[i]=false;
-            
+    public static void permutation(int [] nums, boolean[] vis, List<List<Integer>> ans, List<Integer> a){
+        int n= nums.length;
+        for(int i=0;i<n;i++){
+        if(a.size()==n){
+            List<Integer> copy= new ArrayList<> (a);
+            ans.add(copy);
+            return;
         }
-                }
+            if(!vis[i]){
+                a.add(nums[i]);
+                vis[i]=true;
+                permutation(nums, vis, ans, a);
+                a.remove(a.size()-1);
+                vis[i]=false;
+            }
+
+
+        }
     }
     public List<List<Integer>> permute(int[] nums) {
         int n= nums.length;
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> ds= new ArrayList<>();
+        boolean [] vis=new boolean [n];
+        List<List<Integer>> ans= new ArrayList<>();
+        List<Integer> a= new ArrayList<>();
 
-        boolean [] freq= new boolean[n];
-
-        allPermute(nums,ds,ans, freq);
+        permutation(nums, vis, ans, a );
         return ans;
-    
-
-    
-
         
     }
 }
