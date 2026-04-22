99e0b021912a8e2bf30a690bc8bb52be85c41f1a
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-04-23 01:55:07 +0530

    Time: 819 ms (45.39%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -3,7 +3,7 @@ Select person_name from (
     Select person_name , sum(weight) over (order by turn) as total_weight
     from Queue 
 
-)t 
+) as t
 where total_weight <=1000
 order by total_weight desc
 limit 1
