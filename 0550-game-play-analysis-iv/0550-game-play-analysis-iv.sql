72754b65babd02109001ce8ca98ceabf66dd6d7b
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-06-10 19:35:26 +0530

    Time: 765 ms (12.4%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,13 +1,12 @@
 # Write your MySQL query statement below
-with filtered as (
-    Select Activity.player_id ,Activity.event_date , min(Activity.event_date) as first_date
-    from Activity
-    group by player_id 
+with filterd as (
+    Select Activity.player_id , Activity.event_date, min(Activity.event_date) as first_date
+    from Activity 
+    group by Activity.player_id
 )
 
-Select Round(coalesce(Sum(case when datediff(Activity.event_date,filtered.first_date)=1 then 1  End )/
-count(distinct Activity.player_id),0), 2) as fraction
+Select round(coalesce(Sum(case when datediff(Activity.event_date, filterd.first_date)=1 then 1 End)/
+count(distinct Activity.player_id),0),2) as fraction 
 from Activity
 
-join filtered
-on Activity.player_id=filtered.player_id
+join filterd on Activity.player_id = filterd.player_id
