dc789d91677cbbb5218b1bdb2b495b13dff31666
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-06-10 14:55:51 +0530

    Time: 16 ms (3.55%), Space: 63.9 MB (7.94%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,21 +1,25 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k %= n;
-        reverse(nums, 0, n - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, n - 1);
-    }
+        Stack<Integer> st1= new Stack<>();
+                Stack<Integer> st2= new Stack<>();
 
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
+        int n= nums.length;
+        k=k%n;
+        for(int i=n-1;i>=0;i--){
+            if(i>=n-k) st1.push(nums[i]);
+            else st2.push(nums[i]);
+
         }
+        // nums[]= new int[n];
+        int i=0;
+        while(st1.size()!=0){
+            nums[i++]= st1.pop();
+            
+        }
+        while(st2.size()!=0){
+            nums[i++]=st2.pop();
+        }
+
+        
     }
 }
-
-                                                                                        
