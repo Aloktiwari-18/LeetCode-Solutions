cee25d66bf136ad6feedc10c66d5b63b96dfda10
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-06-15 15:22:59 +0530

    Time: 1 ms (87.37%), Space: 45.7 MB (29.61%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,30 +1,26 @@
 class Solution {
-    public static void permutation(int [] nums, boolean[] vis, List<List<Integer>> ans, List<Integer> a){
+    public static void f(int nums[], boolean [] vis, List<Integer> temp, List<List<Integer>> ans){
         int n= nums.length;
         for(int i=0;i<n;i++){
-        if(a.size()==n){
-            List<Integer> copy= new ArrayList<> (a);
-            ans.add(copy);
-            return;
-        }
+            if(temp.size()==nums.length){
+                ans.add(new ArrayList<>(temp));
+                return;
+            }
             if(!vis[i]){
-                a.add(nums[i]);
+                temp.add(nums[i]);
                 vis[i]=true;
-                permutation(nums, vis, ans, a);
-                a.remove(a.size()-1);
+                f(nums, vis, temp,ans);
+                temp.remove(temp.size()-1);
                 vis[i]=false;
             }
-
-
         }
     }
     public List<List<Integer>> permute(int[] nums) {
         int n= nums.length;
-        boolean [] vis=new boolean [n];
         List<List<Integer>> ans= new ArrayList<>();
-        List<Integer> a= new ArrayList<>();
-
-        permutation(nums, vis, ans, a );
+        boolean [] vis= new boolean [n];
+        List<Integer> temp= new ArrayList<>();
+        f(nums, vis, temp, ans);
         return ans;
         
     }
