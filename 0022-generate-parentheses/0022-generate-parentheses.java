d97106f11b58b245b819bd38aa801376995129a5
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-06-15 16:24:54 +0530

    Time: 0 ms (100%), Space: 44.2 MB (92.81%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static void f(int n, StringBuilder temp, int open ,int close, List<String> ans){
+        if(close>n || close>open || open > n) return ;
+        if(temp.length()==2*n){
+            ans.add(temp.toString());
+        }
+        if(open<n){
+            temp.append("(");
+            f(n, temp, open+1, close, ans );
+            temp.deleteCharAt(temp.length()-1);
+        }
+        if(close<open){
+            temp.append(")");
+            f(n, temp, open, close+1, ans );
+            temp.deleteCharAt(temp.length()-1);
+        }
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> ans= new ArrayList<>();
+        StringBuilder sb= new StringBuilder();
+        f(n, sb, 0, 0, ans);
+        return ans;
+        
+    }
+}
