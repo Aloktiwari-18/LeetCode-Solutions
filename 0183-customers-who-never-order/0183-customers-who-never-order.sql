aa607d2039acef7075a1a28f1e03fe3ba2f3216d
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-06-25 22:56:37 +0530

    Time: 1134 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,12 +1,13 @@
 # Write your MySQL query statement below
-with never_order as (
-    Select 
-   
-    Customers.name 
-    from Customers
-    left join Orders
-    on Customers.id =Orders.customerId
-    Where Orders.customerId is null
+
+with filterd as (
+Select name from Customers
+left join Orders on Customers.id=Orders.customerId
+ Where Orders.customerId is null
 )
-Select name as Customers 
-from never_order
+
+Select name as Customers
+from filterd 
+
+
+
