850f149913577c1a07252f290f966e7851c4f360
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-07-02 14:29:55 +0530

    Time: 41 ms (20.18%), Space: 97.7 MB (89.05%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        Stack<Integer> st= new Stack<>();
+        int t= Integer.MIN_VALUE;
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i]<t){
+                return true;
+            }
+            while(st.size()!=0 && st.peek()<nums[i]){
+               t= st.pop();
+                
+            }
+            st.push(nums[i]);
+        }
+        return false;
+        
+    }
+}
