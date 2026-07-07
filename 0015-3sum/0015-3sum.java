9ac01ad3e93fed02fa6c4bdd799cc9f607e066ac
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-07-07 18:32:24 +0530

    Time: 41 ms (20.4%), Space: 60.5 MB (7.74%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -7,7 +7,8 @@ class Solution {
         for(int i=0;i<n-2;i++){    
             if(i>0 && nums[i]==nums[i-1]){
                 continue;
-            }        
+            }     
+            if(nums[i]>0) break;   
             int sum= -1*nums[i];
             int left=i+1;
             int right=n-1;
