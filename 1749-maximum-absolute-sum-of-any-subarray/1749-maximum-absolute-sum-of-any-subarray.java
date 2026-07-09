2532a2bb88996c18d14e548155de2bd5f7e9cdd4
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-07-09 23:28:57 +0530

    Time: 7 ms (59.32%), Space: 66.5 MB (96.43%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.java
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.java
@@ -3,23 +3,14 @@ class Solution {
         int n= nums.length;
         int bestEnd=nums[0];
         int res=nums[0];
-        for(int i=1;i<n;i++){
-            bestEnd= Math.max(bestEnd+nums[i], nums[i]);;
-
-
-            res= Math.max(res, bestEnd);
-
-
-        } 
-
         int worstEnd=nums[0];
         int abs= nums[0];
         for(int i=1;i<n;i++){
+            bestEnd= Math.max(bestEnd+nums[i], nums[i]);;
+            res= Math.max(res, bestEnd);
             worstEnd= Math.min(worstEnd+nums[i], nums[i]);
             abs= Math.min(abs, worstEnd);
-
-        }
-        return Math.max(res, Math.abs(abs));
-       
+        }  
+        return Math.max(res, Math.abs(abs));   
     }
 }
