9b5670343103fb954b298fa58f55ada9971372a4
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-07-27 15:27:07 +0530

    Time: 1927 ms (5%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -4,9 +4,7 @@ with filterd as (
     from Activity 
     group by Activity.player_id
 )
-
-Select round(coalesce(Sum(case when datediff(Activity.event_date, filterd.first_date)=1 then 1 End)/
+Select round(ifnull(Sum(case when datediff(Activity.event_date, filterd.first_date)=1 then 1 End)/
 count(distinct Activity.player_id),0),2) as fraction 
 from Activity
-
 join filterd on Activity.player_id = filterd.player_id
