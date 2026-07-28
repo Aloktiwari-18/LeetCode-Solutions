4e8f972194407c4341c48c942c2c85751adade8a
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-07-29 00:48:59 +0530

    Time: 1 ms (63.39%), Space: 43.1 MB (72.4%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numJewelsInStones(String j, String s) {
+        HashSet<Character> set= new HashSet<>();
+        for(int i=0;i<j.length();i++){
+            set.add(j.charAt(i));
+            
+        }
+        int count=0;
+         for(int i=0;i<s.length();i++){
+            if(set.contains(s.charAt(i))){
+                count++;
+            }
+            
+        }
+        return count;
+    }
+}
