0cb77b27b2616e366adac053030fedd5905b8a62
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-08-03 00:02:54 +0530

    Time: 0 ms (100%), Space: 43.3 MB (35.84%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,32 +1,6 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        int aliceFront =0;
-        int aliceBack=0;
-        int bobFront=0;
-        int bobBack=0;
-
-        int st=0;
-        int end=piles.length-1;
-        for(int i=st;i<end;i++){
-            if(i%2==0){
-                aliceFront+=piles[i];
-            }else{
-                bobFront+=piles[i];
-            }
-        }
-        for(int i=end;i>=0;i--){
-            if(i%2!=0){
-                aliceBack+=piles[i];
-            }else{
-                bobBack+=piles[i];
-            }
-        }
-
-        if(aliceFront>bobFront || aliceBack>bobBack){
-            return true;
-        }else{
-            return false;
-        }
+        return true;
         
     }
 }
