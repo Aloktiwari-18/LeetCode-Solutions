61bd37b0c0372fc1487ff2486f796cef088e1bdb
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-08-03 15:01:56 +0530

    Time: 39 ms (16.11%), Space: 64.2 MB (10.32%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.java
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.java
@@ -1,19 +1,35 @@
 class Solution {
+     int cnt=0;
+    public void helper(int num1, int num2){
+        if(num1>num2){
+            return ;
+        }
+        String s= String.valueOf(num1);
+        for(int i=1;i<s.length()-1;i++){
+           if(s.charAt(i)>s.charAt(i-1) && s.charAt(i)>s.charAt(i+1) 
+                 || s.charAt(i)<s.charAt(i-1) && s.charAt(i)<s.charAt(i+1)){
+                     cnt++;
+                 }
+        }
+        helper(num1+1, num2);
+    }
     public int totalWaviness(int num1, int num2) {
-        int cnt=0;
-       for(int x=num1; x<=num2;x++){
-            String s= String.valueOf(x);
-            for(int i=1;i<s.length()-1;i++){
-                if(s.charAt(i)>s.charAt(i-1) && s.charAt(i)>s.charAt(i+1) 
-                || s.charAt(i)<s.charAt(i-1) && s.charAt(i)<s.charAt(i+1)){
-                    cnt++;
-                }
+    //     int cnt=0;
+    //    for(int x=num1; x<=num2;x++){
+    //         String s= String.valueOf(x);
+    //         for(int i=1;i<s.length()-1;i++){
+    //             if(s.charAt(i)>s.charAt(i-1) && s.charAt(i)>s.charAt(i+1) 
+    //             || s.charAt(i)<s.charAt(i-1) && s.charAt(i)<s.charAt(i+1)){
+    //                 cnt++;
+    //             }
 
-            }
-       }
-       return cnt;
-        
+    //         }
+    //    }
+    //    return cnt;
+   
 
+     helper(num1, num2);
+     return cnt;
         
     }
 }
