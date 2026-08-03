ee331823094590ba4e0c5b58dba8f242cea1559a
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-08-03 16:27:44 +0530

    Time: 0 ms (100%), Space: 42.6 MB (19.46%) - LeetHub

--- 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.java
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.java
@@ -1,22 +1,6 @@
 class Solution {
-    public int helper(int n, int dp[]){
-        if(n==1) return 0;
-
-        if(dp[n]!=Integer.MIN_VALUE){
-            return dp[n];
-        }
-        int ans=Integer.MAX_VALUE;
-        for(int a=1;a<n;a++){
-            int b= n-a;
-            ans= Math.min(ans, a*b +helper(a, dp)+helper(b,dp));
-
-        }
-        return dp[n]=ans;
-    }
     public int minCost(int n) {
-        int dp[]= new int[n+1];
-        Arrays.fill(dp, Integer.MIN_VALUE);
-        return helper(n, dp);
+        return (n*(n-1))/2;
         
     }
 }
