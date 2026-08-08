6b3517a367f3fcd07c4431125c9626a5d26d4455
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-08-08 19:23:23 +0530

    Time: 1 ms (10.48%), Space: 46.3 MB (83.05%) - LeetHub

---
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int smallestEqual(int[] nums) {
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(i%10==nums[i]){
+                min=Math.min(i,min);
+            }
+        }
+        if(min==Integer.MAX_VALUE){
+            return -1;
+        }
+        return min;
+        
+    }
+}
