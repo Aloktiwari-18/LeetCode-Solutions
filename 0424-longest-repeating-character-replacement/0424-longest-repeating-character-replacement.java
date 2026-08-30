1d9a4d36e54d41cf1cf9c6f2d34685e423ce787d
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-08-30 23:35:49 +0530

    Time: 6 ms (98.89%), Space: 45.9 MB (90.83%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,35 +1,5 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-                       // BRUTE FORCE 
-        // int maxlen=0;
-        
-        // for(int i=0;i<s.length();i++){
-        //     int freq[] = new int[26];
-        //     int maxfreq=0;
-        //     int changes=0;
-
-        //     for(int j=i;j<s.length();j++){
-        //         int idx=s.charAt(j)-'A';
-        //         freq[idx]++;
-               
-        //         maxfreq=Math.max(maxfreq,freq[idx]);
-
-        //         changes=(j-i+1)-maxfreq;
-        //         if(changes<=k){
-        //             maxlen=Math.max(maxlen,(j-i+1));
-
-        //         }else{
-        //             break;
-        //         }
-
-        //     }
-        // }
-        // return maxlen;
-
-
-
-        //                     SLIDING WINDOW METHOD
-
         int left=0;
         int right=0;
         int n=s.length();
@@ -47,14 +17,14 @@ class Solution {
                     maxfreq=Math.max(maxfreq,hash[i]);
                 }
                 left=left+1;
-            }if((right-left+1)-maxfreq<=k){
+            }
+            if((right-left+1)-maxfreq<=k){
                 maxlen=Math.max(maxlen,right-left+1);
             }
             right++;
 
         }
         return maxlen;
-
         
     }
 }
