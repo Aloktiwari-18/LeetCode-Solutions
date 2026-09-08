479a7426fbb24d51ee4c0d34092ed60feabe52ad
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-09-08 15:19:48 +0530

    Time: 1 ms (83.65%), Space: 42.6 MB (57.08%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,25 +1,6 @@
 class Solution {
     public int countCommas(int n) {
-        long totalCommas = 0;
-        long currentPowerOfThousand = 1000;
-        int commasPerNumber = 1;
-
-        while (currentPowerOfThousand <= n) {
-            long rangeStart = currentPowerOfThousand;
-            long rangeEnd = currentPowerOfThousand * 1000 - 1;
-
-            if (rangeEnd > n) {
-                rangeEnd = n;
-                totalCommas += (rangeEnd - rangeStart + 1) * commasPerNumber;
-                break;
-            } else {
-                totalCommas += (rangeEnd - rangeStart + 1) * commasPerNumber;
-            }
-            
-            currentPowerOfThousand *= 1000;
-            commasPerNumber++;
-        }
-
-        return (int) totalCommas;
+        int ans= Math.max(0,n-999);
+        return ans;
     }
 }
