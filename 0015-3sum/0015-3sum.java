5c071f3fb9ea5d91ad98f0d7ee73f8f1af3da04f
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-09-12 01:01:34 +0530

    Time: 31 ms (81.5%), Space: 59.2 MB (59.59%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,42 +1,39 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        
+        Arrays.sort(nums);
         List<List<Integer>> ans= new ArrayList<>();
-        int n=nums.length;
-        if(n<3) return ans;    
-        Arrays.sort(nums); 
-        for(int i=0;i<n-2;i++){    
+        if(nums.length<3){
+            return ans;
+        }
+        for(int i=0;i<nums.length-2;i++){
             if(i>0 && nums[i]==nums[i-1]){
                 continue;
-            }     
-            if(nums[i]>0) break;   
-            int sum= -1*nums[i];
-            int left=i+1;
-            int right=n-1;
-            while(left<right){
-                List<Integer> sub= new ArrayList<>();
-                int s= nums[left]+nums[right];
-                if(s==sum){
-                    sub.add(nums[i]);
-                    sub.add(nums[left]);
-                    sub.add(nums[right]);
-                    ans.add(sub);
-                    left++;
-                    right--;
-                    while(left<n && nums[left]==nums[left-1]){
-                        left++;
-                    }
-                    while(right>=0 && nums[right]==nums[right+1]){
-                        right--;
-                    }
-                    
-                }
-                else if(s<sum){
-                    left++;
-                }else{
-                    right--;
-                }    
             }
+            int j=i+1;
+            int k=nums.length-1;
+            
+
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                
+                if(sum<0){
+                    j++;
+                }
+              else  if(sum>0){
+                k--;
+              }else{
+                ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                j++;
+                k--;
+                 while(j<k && nums[j]==nums[j-1]) j++;
+              while(j<k && nums[k]==nums[k+1]) k--;
+              }
+             
+            }
+
         }
-        return ans;    
+        return ans;
+        
     }
 }
