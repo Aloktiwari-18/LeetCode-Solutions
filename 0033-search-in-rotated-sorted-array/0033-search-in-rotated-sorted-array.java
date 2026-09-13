bde6c02bdfdc2ec50353b9ba511a5af74e966b50
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-09-13 18:20:39 +0530

    Time: 0 ms (100%), Space: 43.9 MB (47.05%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,35 +1,39 @@
 class Solution {
     int ans=-1;
-    public void funct(int [] nums, int st, int end, int target){
-            if(st>end) return;
-         int mid=(st+end)/2;
-            if(nums[mid]==target){
-               ans= mid;
-               return ;
-            }
-            if(nums[st]<=nums[mid]){
-                if(target>=nums[st]  && target<=nums[mid]){
-                    funct(nums, st, mid-1, target);
+    public void solve(int [] nums, int st, int end, int target){
+        int mid= (st+end)/2;
+        if(st>end){
+            return ;
+        }
+        if(nums[mid]==target){
+          ans=mid;
+          return;
+        }
+        
+
+        if(nums[st]<=nums[mid]){
+                if(target>=nums[st] && target<=nums[mid]){
+                    solve(nums, st, mid-1,target);
                 }else{
-                    funct(nums, mid+1, end, target);
+                    solve(nums, mid+1, end,target);
+               
                 }
-
             }
-            else{
-                if(target>=nums[mid]  && target<=nums[end]){
-                    funct(nums, mid+1, end, target);
-                }else{
-                    funct(nums, st, mid-1, target);
-                }
-
+        else{
+            if(target>=nums[mid] && target<=nums[end]){
+                solve(nums, mid+1, end,target);
+            }else{
+                solve(nums, st, mid-1 ,target);
             }
-
- }
+        }
+       
+    }
     public int search(int[] nums, int target) {
-       funct(nums, 0, nums.length-1, target);
-       return ans;
+         solve(nums, 0 ,nums.length-1,target);
+         return ans;
+        
+            
+        
         
     }
 }
-
-
