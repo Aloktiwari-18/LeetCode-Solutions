63c60a7fb988f19b81ef85ac12d3af3d41081c44
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-09-19 00:50:11 +0530

    Time: 2 ms (41.52%), Space: 45 MB (78.4%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -13,43 +13,39 @@
  *     }
  * }
  */
-
- class Pair{
-
+class Pair
+{
     TreeNode node;
-    int num;
-    Pair(TreeNode node, int num){
+    int idx;
+    Pair(TreeNode node, int idx){
         this.node= node;
-        this.num= num;
+        this.idx=idx;
     }
- }
+}
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        int ans=0;
-        if(root==null) return 0;
         Queue<Pair> q= new LinkedList<>();
         q.add(new Pair(root,0));
-        while(q.size()>0){
+        int max=Integer.MIN_VALUE;
+        while(!q.isEmpty()){
             int size=q.size();
-            int f=0;
-            int l=0;
-            int min=q.peek().num;
-
+            Pair curr= q.peek();
+            int f= curr.idx;
+            int s=0;
             for(int i=0;i<size;i++){
-                Pair nodes= q.poll();
-                TreeNode node= nodes.node;
-                int currId= nodes.num;
-
-
-                if(i==0) f=currId;
-                if(i==size-1) l=currId;
-                if(node.left!=null) q.add(new Pair(node.left, 2*currId+1));
-                if(node.right!=null) q.add(new Pair(node.right, 2*currId+2));
+                Pair next= q.poll();
+                TreeNode next1=next.node;
+                s=next.idx;
+                if(next1.left!=null){
+                    q.add(new Pair(next1.left,2*s));
+                }
+                if(next1.right!=null){
+                    q.add(new Pair(next1.right,2*s+1));
+                }
             }
-            ans=Math.max(ans, l-f+1);
-
+            max= Math.max(max, s-f+1);
         }
-         return ans;
+        return max;
         
     }
 }
