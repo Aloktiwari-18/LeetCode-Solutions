6f890bb1337b27e4f8c8f631938c88bb88af6902
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-09-20 20:56:54 +0530

    Time: 20 ms (63.58%), Space: 113.9 MB (34.88%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.java
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.java
@@ -15,41 +15,49 @@
  */
 class Solution {
     public TreeNode replaceValueInTree(TreeNode root) {
-        if (root == null) return null;
+        Queue<TreeNode> q= new LinkedList<>();
+        q.add(root);
+        ArrayList<Integer> arr= new ArrayList<>();
+        while(!q.isEmpty()){
+            int size=  q.size();
+            int levelSum= 0;
+            for(int i= 0;i<size; i++){
+                TreeNode curr= q.poll();
+                levelSum+=curr.val;
+                if(curr.left!=null){
+                    q.add(curr.left);
+                }
+                if(curr.right!=null){
+                    q.add(curr.right);
+                }
 
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        root.val = 0; 
 
-        while (!q.isEmpty()) {
-            int size = q.size();
-            int levelSum = 0;
-
-            List<TreeNode> parents = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                TreeNode curr = q.poll();
-                parents.add(curr);
-
-                if (curr.left != null) levelSum += curr.left.val;
-                if (curr.right != null) levelSum += curr.right.val;
             }
-
+            arr.add(levelSum);
+        }
+        q.add(root);
+        root.val=0;
+        int level=1;
+        while(!q.isEmpty()){
+            int size= q.size();
             
-            for (TreeNode parent : parents) {
-                int siblingSum = 0;
-                if (parent.left != null) siblingSum += parent.left.val;
-                if (parent.right != null) siblingSum += parent.right.val;
+            for(int i=0;i< size;i++){
+                TreeNode curr= q.poll();
+                int sibling= curr.left!=null ? curr.left.val:0;
+                sibling+=curr.right!=null ? curr.right.val:0;
 
-                if (parent.left != null) {
-                    parent.left.val = levelSum - siblingSum;
-                    q.offer(parent.left);
+                if(curr.left!=null){
+                    curr.left.val= arr.get(level)- sibling;
+                    q.add(curr.left);
                 }
-                if (parent.right != null) {
-                    parent.right.val = levelSum - siblingSum;
-                    q.offer(parent.right);
+                if(curr.right!=null){
+                    curr.right.val= arr.get(level)-sibling;
+                    q.add(curr.right);
                 }
             }
+            level++;
         }
         return root;
+        
     }
 }
