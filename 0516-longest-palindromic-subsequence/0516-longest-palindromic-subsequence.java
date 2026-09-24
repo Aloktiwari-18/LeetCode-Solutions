06f716fe616b4b82e6ba6720172f698dcb4cea06
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-09-24 17:56:13 +0530

    Time: 106 ms (5.32%), Space: 63.9 MB (48.75%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,39 +1,32 @@
 class Solution {
-       static int [][]dp;
-    public int lcs(int i, int j, StringBuilder a  , StringBuilder b ){
-        if(i<0 || j<0) return 0;
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        if(a.charAt(i)==b.charAt(j)){
-            return dp[i][j]=1+ lcs(i-1, j-1,a, b);
-        }else
-            return dp[i][j]= Math.max(lcs(i-1, j , a, b),lcs(i, j-1, a, b));
+    // public int solve(int m, int n, StringBuilder sb1, StringBuilder sb2, int [] dp){
+    //     if(m==0 || n==0){
+    //         return 0;
+    //     }
+    //     if(sb1.charAt(i)==sb2.charAt())
+    // }
+   
+    public int longestPalindromeSubseq(String s) {
         
-
-    }
-    public int longestCommonSubsequence(String text1, String text2) {
-        StringBuilder a = new StringBuilder(text1);
-        StringBuilder b = new StringBuilder(text2);
-
-        int m= a.length();
-        int n= b.length();
-        dp= new int[m][n];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                dp[i][j]= -1;
+        StringBuilder sb1 = new StringBuilder(s);
+        StringBuilder s2= new StringBuilder(s);
+        StringBuilder sb2= s2.reverse();
+        int dp[][]= new int[sb1.length()+1][sb2.length()+1];
+        
+      for(int i=1;i<=sb1.length();i++){
+        for(int j=1;j<=sb2.length();j++){
+            if(sb1.charAt(i-1)==sb2.charAt(j-1)){
+                dp[i][j]= dp[i-1][j-1]+1;
+            }else{
+                dp[i][j]=Math.max(dp[i-1][j], dp[i][j-1]);
             }
         }
+      }
+      return dp[sb1.length()][sb2.length()];
+        
 
-        return lcs(m-1, n-1, a, b);
-    } 
-    public String reverse(String s){
-        StringBuilder sb= new StringBuilder(s);
+       
 
-        return sb.reverse().toString();
-    } 
-    public int longestPalindromeSubseq(String s) {
-        return longestCommonSubsequence(s, reverse(s));
         
     }
 }
